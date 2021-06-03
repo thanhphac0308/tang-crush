30fcaeb86dfec4e250a87fafec01119a60400930
thanhphac0308 <85274245+thanhphac0308@users.noreply.github.com>
2021-06-03 13:21:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
     introTitle: 'Thủy lợn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Trên trời có triệu vì sao
+    Xếp thành bốn chữ vì sao yêu nàng
+    Dưới đất có vạn con người
+    Mà sao chỉ có một người tôi yêu`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -11,9 +11,9 @@ const CONFIG = {
     btnNo: 'Không, Fat à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho Fat <3',
-    reply: '',
+    reply: 'Vì mày đáng yêu <3333',
     mess: 'Fat biết mà 🥰. Yêu Thủy nhiều nhiều 😘😘',
     messDesc: 'Yêu công chúa.',
     btnAccept: 'Yêu Fat <3 ',
-    messLink: 'http://fb.com/thanhphac0308' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/thanhphac0308' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
